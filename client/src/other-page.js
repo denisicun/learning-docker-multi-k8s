3693d63108e7dcbfc297fdadce5f51fe15578d35
denissicun <D@nis2609>
2021-01-29 10:58:05 +0200

    initial commit

---
+++ client/src/other-page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+// eslint-disable-next-line
+export default () => {
+	return (
+		<div>
+			Yo Yo!!
+			<Link to={"/"}>Go Back Home</Link>
+		</div>
+	);
+}
